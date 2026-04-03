08603d9ca690c467252dc59db78094f3cf801ae0
Henry Cipolla <hcipolla@gmail.com>
2026-04-03 11:18:04 -0400

    Removed some unused files

--- apps/web/components/analytics/index.ts
+++
@@ -1,5 +0,0 @@
-export { SkillsHeatmap } from './skills-heatmap';
-export { SpellsChart } from './spells-chart';
-export { StatsOverview } from './stats-overview';
-export { AggregationBuilder } from './aggregation-builder';
-export { TrendsChart } from './trends-chart';
